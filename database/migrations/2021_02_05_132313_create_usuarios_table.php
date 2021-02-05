2c4836dfbe0e6571d1590564a3b96e52effd91b3
Francisca <al221811678@gmail.com>
2021-02-05 08:16:19 -0600

    Migración y seeder de usuarios

---
+++ database/migrations/2021_02_05_132313_create_usuarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->char('id', 3)->comment('ID');
+            $table->string('nombre', 50)->comment('Nombre');
+            $table->string('primer_apellido', 50)->comment('Primer_apellido');
+            $table->string('direccion', 50)->comment('Direccion');
+            $table->integer('telefono', 50)->comment('Telefono');
+            $table->string('correo_electronico', 50)->comment('Correo_electronico');
+            $table->string('password', 10)->comment('Password');
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
---
+++ database/seeders/UsuariosTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsuariosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('usuarios')->insert([
+            ['id' => 'usu1', 'nombre' => 'Juan', 'primer_apellido' => 'Mendez', 'direccion' => 'Calle zaragoza', 'telefono' => '729359869', 'correo_electronico' => 'juan@gmail.com', 'password' => 'juan123']
+            , ['id' => 'usu2', 'nombre' => 'Monica', 'primer_apellido' => 'Torres', 'direccion' => 'Calle flores', 'telefono' => '729359868', 'correo_electronico' => 'monica@gmail.com', 'password' => 'monica123']
+            , ['id' => 'usu3', 'nombre' => 'Luis', 'primer_apellido' => 'Sanchez', 'direccion' => 'Calle dalia', 'telefono' => '729359862', 'correo_electronico' => 'luis@gmail.com', 'password' => 'luis123']
+        ]);
+    }
+}
