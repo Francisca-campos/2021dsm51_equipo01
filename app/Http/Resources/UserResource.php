a019820e59986adeaca25a3dd5c0f895a978c08e
Francisca <al221811678@gmail.com>
2021-02-10 11:10:12 -0600

    Token

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,11 +15,15 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'id' => $this->id
+            , 'name' => $this->name
+            , 'primer_apellido' => $this->primer_apellido
+            , 'segundo_apellido' => $this->segundo_apellido
+            , 'sexo' => $this->sexo
+            , 'fecha_nacimiento' => $this->fecha_nacimiento
+            , 'estatus' => $this->estatus
+            , 'perfil' => $this->perfil
+            , 'email' => $this->email
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Auth\AuthenticationException;
+use App\Http\Controllers\Api\UsuariosController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +18,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/usuarios', function (Request $request) {
-    return $request->usuarios();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('usuarios', UsuariosController::class)->except(['create', 'edit']);
+
+});
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
 
 Route::post('/tokens/create', function (Request $request) {
@@ -28,6 +36,8 @@ Route::post('/tokens/create', function (Request $request) {
         throw new AuthenticationException();
     }
     return [
-        'token' => auth()->usuarios()->createToken('test')->plainTextToken
+    'token' => auth()->user()->createToken('test')->plainTextToken
     ];
 });
+
+
